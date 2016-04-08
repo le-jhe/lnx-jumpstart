af22575443878b9e19a96507bbc15f544075ec92
jhe <le.jhe@free.fr>
2016-04-09 00:08:07 +0200

    docker, jdk, mvn

--- 30-ansible.sh
+++ 30-ansible.sh
@@ -1,4 +1,6 @@
 ANSIBLE_PLAYBOOK_HOME=./30-ansible
 ANSIBLE_USER=m999jve
 echo "running with ANSIBLE_PLAYBOOK_HOME=${ANSIBLE_PLAYBOOK_HOME}"
-ansible-playbook -vvvv -s --ask-become-pass ${ANSIBLE_PLAYBOOK_HOME}/docker.yml -u ${ANSIBLE_USER}
+ansible-playbook -s --ask-become-pass ${ANSIBLE_PLAYBOOK_HOME}/docker.yml -u ${ANSIBLE_USER}
+ansible-playbook -s --ask-become-pass ${ANSIBLE_PLAYBOOK_HOME}/jdk.yml -u ${ANSIBLE_USER}
+ansible-playbook -s --ask-become-pass ${ANSIBLE_PLAYBOOK_HOME}/mvn.yml -u ${ANSIBLE_USER}
