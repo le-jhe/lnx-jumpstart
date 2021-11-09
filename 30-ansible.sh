a6745619b6410a7a6b7d93ec64c4cece05e52002
le.jhe <le.jhe@free.fr>
2021-11-09 01:19:07 +0100

    adding http://yatta.de/profiles

--- 30-ansible.sh
+++ 30-ansible.sh
@@ -11,8 +11,10 @@ echo "running with ANSIBLE_PLAYBOOK_HOME=${ANSIBLE_PLAYBOOK_HOME}"
 #sudo ansible -vvvvv -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml --check --diff
 #ansible-playbook -vvvvv -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER} --check --diff --list-hosts
 #ansible-playbook -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER} --check --diff
-ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml -u ${ANSIBLE_USER}
 #ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml
 #ansible-playbook -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER}
 #ansible-playbook --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER}
+# working
+ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml -u ${ANSIBLE_USER}
 ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER}
+ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/yatta.yml -u ${ANSIBLE_USER}
