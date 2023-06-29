5e6cee02d2d14ac2b81ad707971100a40be498dc
jhe <le.jhe@free.fr>
2023-06-29 23:09:45 +0200

    Update 00-manual.sh

--- 00-manual.sh
+++ 00-manual.sh
@@ -1,9 +1,10 @@
+# FIXME : switch between apy & yum / dnf at least.. or pacapt
+# let a variable choose the package manager and do : ${PKG_MANAGER} install xxx
 apt install sudo # if I am root already 
 sudo apt update
 sudo apt upgrade
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update
-sudo apt install -y ansible
-sudo apt install python-is-python3
+sudo apt install -y curl ansible python-is-python3
 # FIXME : could be installed via ansible
 sudo apt -y install vim git openssh-server curl
