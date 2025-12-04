760093e3d38ba784c14a4849ba27ac5931672b40
le-jhe <le.jhe@free.fr>
2025-12-04 21:14:43 +0100

    working again

--- 30-ansible.sh
+++ 30-ansible.sh
@@ -1,6 +1,6 @@
 #ANSIBLE_PLAYBOOK_HOME=${PWD}/30-ansible
 ANSIBLE_PLAYBOOK_HOME=./30-ansible
-ANSIBLE_USER=jhe
+#ANSIBLE_USER=jhe
 ANSIBLE_CONFIG=${ANSIBLE_PLAYBOOK_HOME}/ansible.cfg
 export ANSIBLE_PLAYBOOK_HOME ANSIBLE_USER ANSIBLE_CONFIG
 echo "running with ANSIBLE_PLAYBOOK_HOME=${ANSIBLE_PLAYBOOK_HOME}"
@@ -11,7 +11,8 @@ echo "running with ANSIBLE_PLAYBOOK_HOME=${ANSIBLE_PLAYBOOK_HOME}"
 #ansible-playbook -vvvvv -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER} --check --diff --list-hosts
 #ansible-playbook -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER} --check --diff
 # those are on the way.. --V
-ansible-playbook -vvv -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml -u ${ANSIBLE_USER}
+#ansible-playbook -vvvvvv --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml -u ${ANSIBLE_USER}
+ansible-playbook -vvvvvv --ask-become-pass --become-method=sudo -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local ${ANSIBLE_PLAYBOOK_HOME}/playbooks/pkgs.yml
 #ansible-playbook -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/mvn.yml -u ${ANSIBLE_USER}
-ansible-playbook -vvv -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/jdk.yml -u ${ANSIBLE_USER}
+#ansible-playbook -s --ask-become-pass -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/jdk.yml -u ${ANSIBLE_USER}
 #sudo ansible-playbook -i ${ANSIBLE_PLAYBOOK_HOME}/inventories/local -l localhost ${ANSIBLE_PLAYBOOK_HOME}/playbooks/jdk.yml
